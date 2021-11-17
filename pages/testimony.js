56221e42e9c72e5a3634ef0c461905e35608e0fc
Sebastianus Sembara <sembara9090@gmail.com>
2021-11-18 05:16:22 +0700

    modified testimony pages

--- pages/testimony.js
+++ pages/testimony.js
@@ -48,7 +48,14 @@ const Testimony = ({ testimonies }) => {
 };
 
 export async function getStaticProps() {
-  const res = await fetch('http://zeylosociale.vercel.app//api/testimonies');
+  const res = await fetch('http://zeylosociale.vercel.app//api/testimonies', {
+    method: 'GET',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+      Accept: 'application/json; charset=UTF-8'
+    }
+  });
   const testimonies = await res.json();
   return {
     props: {
